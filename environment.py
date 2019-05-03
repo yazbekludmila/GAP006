fe01300e6ee99d188c8ffa2f22b575d0e78c9389
Joao Victor <jvictorr@indracompany.com>
2019-05-03 18:30:47 -0300

    commit initial

---
+++ environment.py
@@ -0,0 +1,31 @@
+
+"""		Pyautomator Framework de teste 
+
+			TRONWEB"""
+
+from Pyautomators import *
+from time import sleep
+def before_all(context):
+	pass
+
+def before_feature(context,feature):
+	pass
+
+def before_scenario(context,scenario):
+	pass
+
+def before_step(context,step):
+	pass
+
+def after_step(context,step):
+	pass
+
+def after_scenario(context,scenario):
+	pass
+
+def after_feature(context,feature):
+	pass
+
+def after_all(context):
+	pass
+
